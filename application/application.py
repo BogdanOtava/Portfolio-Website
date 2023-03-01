af93a094668f442410fc3116687e336779710d58
BogdanOtava <bogdanotava097@gmail.com>
2023-03-01 19:19:20 +0200

    Added - Flask Setup & Basic Config

---
+++ application/application.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from config import TEMPLATES_PATH
+
+app = Flask(__name__, template_folder=TEMPLATES_PATH)
+
+
+@app.route("/")
+def index():
+    """Renders the homepage of the website. """
+
+    return render_template("index.html")
---
+++ config.py
@@ -0,0 +1,5 @@
+import pathlib
+
+ROOT = pathlib.Path(__file__).parent
+
+TEMPLATES_PATH = ROOT.joinpath("application/templates")
